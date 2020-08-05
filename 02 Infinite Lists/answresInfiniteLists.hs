6cfe2204e1730f00ef7f56f997cb960a88857e47
Bruno Martins <brunorodrigues929@gmail.com>
2020-08-05 18:18:17 -0300

    InfiniteList incomplete fechoKleene and goldbach

--- 02 Infinite Lists/answresInfiniteLists.hs
+++ 02 Infinite Lists/answresInfiniteLists.hs
@@ -13,20 +13,28 @@ merge (x:xs) (y:ys)
   | otherwise = y:merge (x:xs) ys 
 
 -- collatz
-
 collatz x
   | x == 1 = [1]
   | mod x 2==0 = x:collatz (div x 2)
   | mod x 2==1 = x: collatz((x*3)+1)
 
-  -- :collatz (round(n/2))
-  -- | mod n 2==1 = round ((n*3)+1) -- :collatz(round((n*3)+1)) 
-
 
 
 -- fechoKleene
+
 -- goldbach
+
+
+
 -- primosPalindromos
+primosPalindromo = [ z | z<-[1..], isPrimoPali z, isPalindromo z] 
+isPrimoPali x  = if length([y | y<-[1..x], mod x y ==0]) == 2 then True else False
+isPalindromo n = show n == reverse (show n)
+
 -- primosGemeos
+primosGemeos =  [ (a,b) | (a,b)<-(zip (tail [ a | a<-[1..], isPrimo a]) (tail (tail [ a | a<-[1..], isPrimo a]))), b-a==2 ]
+isPrimo x  = if length([y | y<-[1..x], mod x y ==0]) == 2 then True else False
+
+
 
 
