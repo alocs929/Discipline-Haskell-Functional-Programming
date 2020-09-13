30ed666d3ac4975507d2cb9d52de168752a9f993
Bruno Martins <brunorodrigues929@gmail.com>
2020-09-13 15:39:06 -0300

    06 Creating Your Modules Incomplete

--- 06 CreatingYourModules/answersCreatingYourModules.hs
+++ 06 CreatingYourModules/answersCreatingYourModules.hs
@@ -30,7 +30,7 @@ simula n a xs | size (a) == 1 = xs ++ [front a]
 
 
 --[Módulo Stack]NotaçãoPolonesaReversa
-
+--Sem tentativa! Desculpa :'(
 
 
 --[Modulo Data.Set] isograma
@@ -46,22 +46,34 @@ isograma xs = Set.size s == length zs
 --[Módulo Data.Set e Data.Char]pangrama
 import Data.Char
 import qualified Data.Set as Set
-isPanagram text = Set.size (Set.fromList letras) == 26
-  where
-    letras = (filter.map toLower) text
-
+isPangram :: String -> Bool
+isPangram text = Set.size (Set.fromList letras) == 26
+  where letras = (filter isLetter . map toLower) text
 
 
 --[Módulo Data.Set] numeroTrocas
-
+--Sem tentativa! Desculpa :'(
 
 
 --[Data.Map] Palavras palindrômicas
+--Sem tentativa! Desculpa :'(
 
 
 --[Data.Map] transformaAnagrama
+import Data.Char
+import qualified Data.Map as Map
+transformaAnagrama xs ys = sum [ abs(b) | (a,b) <- Map.toList h2]
+  where  
+    processa1 hash xs = foldr (\x acc -> Map.insertWith (+) x   1 acc) hash xs
+    processa2 hash ys = foldr (\x acc -> Map.insertWith (+) x (-1) acc) hash ys
+    h1 = processa1 Map.empty xs
+    h2 = processa2 h1 ys
 
 
 --[Data.Map] duplicates
-
-
+import Data.Char
+import qualified Data.Map as Map
+duplicates xs = [ a | (a,b) <- Map.toList h1, b > 1]
+  where
+  processa hash xs = foldr (\x acc -> Map.insertWith (+) x   1 acc) hash xs
+  h1 = processa Map.empty xs
